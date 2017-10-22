839dd3be36cb901c300ec249fc5640d245b77fab
Grant Wuerker <gwuerker@googlemail.com>
2017-10-22 15:41:51 -0500

    determinant

---
+++ linear-algebra/basic-operations.hs
@@ -0,0 +1,17 @@
+module Gla where
+
+-- determinant
+negateOdds :: [Int] -> [Int]
+negateOdds list = map (\(x,y) -> x * y) (zip (map (\x -> (-1)^x) [0..(length list) - 1]) list)
+
+subMatrices :: [[Int]] -> [[[Int]]]
+subMatrices matrix = foldr (\n accum -> subMatrix n matrix : accum) [] [0..(length matrix) - 1]
+
+subMatrix :: Int -> [[Int]] -> [[Int]]
+subMatrix n (first:rest) = map (\xs -> removeN n xs) rest
+
+removeN n xs = let (ys,zs) = splitAt n xs   in   ys ++ (tail zs) 
+
+determinant :: [[Int]] -> Int
+determinant [[a,b],[c,d]] = a * d - b * c
+determinant matrix = sum (map (\(scalar, matrix) -> scalar * (determinant matrix)) (zip (negateOdds (matrix!!0)) (subMatrices matrix)))
