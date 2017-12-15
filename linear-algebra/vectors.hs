0ac72f2214e441230a7180c815540b757b9ba476
Grant Wuerker <gwuerker@googlemail.com>
2017-12-15 15:03:21 -0600

    converted vector operations from nums to polys

--- linear-algebra/vectors.hs
+++ linear-algebra/vectors.hs
@@ -1,17 +1,33 @@
 module Gla.Vectors where
 
+import Math.Polynomial
+
+-- conversions
+numToPoly :: (Num a, Eq a) => a -> Poly a
+numToPoly x = poly BE [x]
+
+polyToNum :: (Num a, Eq a) => Poly a -> a
+polyToNum p = evalPoly p 0
+
+numsToPolys :: (Num a, Eq a) => [a] -> [Poly a]
+numsToPolys v = map (\x -> numToPoly x) v
+
+polysToNums :: (Num a, Eq a) => [Poly a] -> [a]
+polysToNums v = map (\p -> polyToNum p) v
+
 -- dot product
-dot :: Num a => [a] -> [a] -> a
-dot v1 v2 = sum (map (\(a,b) -> a * b) (zip v1 v2))
+dot :: (Num a, Eq a) => [Poly a] -> [Poly a] -> Poly a
+dot v1 v2 = sumPolys (map (\(a,b) -> multPoly a b) (zip v1 v2))
 
 -- addition
-add :: Num a => [a] -> [a] -> [a]
-add v1 v2 = map (\(a,b) -> a + b) (zip v1 v2)
+add :: (Num a, Eq a) => [Poly a] -> [Poly a] -> [Poly a]
+add v1 v2 = map (\(a,b) -> addPoly a b) (zip v1 v2)
 
 -- subtraction
-subtract :: Num a => [a] -> [a] -> [a]
-subtract v1 v2 = map (\(a,b) -> a - b) (zip v1 v2)
+subtract :: (Num a, Eq a) => [Poly a] -> [Poly a] -> [Poly a]
+subtract v1 v2 = map (\(a,b) -> addPoly a (scalePoly (-1) b)) (zip v1 v2)
 
 -- scale
-scale :: Num a => a -> [a] -> [a]
-scale s v = map (\a -> a * s) v
+scale :: (Num a, Eq a) => Poly a -> [Poly a] -> [Poly a]
+scale s v = map (\a -> multPoly a s) v
+
