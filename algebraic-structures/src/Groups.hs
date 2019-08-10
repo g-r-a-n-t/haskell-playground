51f0cc83d5400b0b9cfd45490c0092a7a29724b3
Grant Wuerker <gwuerker@gmail.com>
2019-08-10 11:33:03 +0200

    abelian groups

--- algebraic-structures/src/Groups.hs
+++ algebraic-structures/src/Groups.hs
@@ -1,6 +1,7 @@
 module Groups (
   newGroup,
-  verify
+  isGroup,
+  isAbelianGroup
 ) where
 
 import Qualities
@@ -8,17 +9,21 @@ import Qualities
 -- Groups contain the following elements: a carrier set `[a]`, an identity element `a`, an inverse operation `(a -> a)`,
 -- and an operation `(a -> a -> a)`.
 data Group a = Group [a] a (a -> a) (a -> a -> a)
-
 newGroup _G e inv op = Group _G e inv op
 
 -- Verifies that the Group elements do in fact form a Group algebra.
 -- This is only computationally feasible on small carrier sets.
-verify :: Ord a => Eq a => Group a -> (Bool, String)
-verify (Group _G e inv op)
-  | not $ hasClosure _G op = (False, "The group does not have closure over opition.")
-  | not $ isAssociative _G op = (False, "Addition is not associative.")
+isGroup :: Eq a => Group a -> (Bool, String)
+isGroup (Group _G e inv op)
+  | not $ hasClosure _G op = (False, "The group does not have closure.")
+  | not $ isAssociative _G op = (False, "The operation is not associative.")
   | not $ hasIdentity _G e op = (False, "The group does not have a valid identity element.")
   | not $ isInvertible _G e inv op = (False, "The group is not invertible.")
   | otherwise = (True, "")
 
-
+isAbelianGroup :: Eq a => Group a -> (Bool, String)
+isAbelianGroup (Group _G e inv op)
+  | not $ isGroupRes = (False, isGroupErr)
+  | not $ isCommunicative _G op = (False, "The group is not communicative.")
+  | otherwise = (True, "")
+  where (isGroupRes, isGroupErr) = isGroup (Group _G e inv op)
--- algebraic-structures/test/GroupsSpec.hs
+++ algebraic-structures/test/GroupsSpec.hs
@@ -7,18 +7,27 @@ import Control.Exception (evaluate)
 import Groups
 import Data.Modular
 
-inv9 :: Mod Integer 9 -> Mod Integer 9
-inv9 a = inv a :: Mod Integer 9
 
-multiplicativeGroupOfIntegersModulo9 = newGroup _G e inv9 mul
+multiplicativeGroupOfIntegersModulo9 = newGroup _G e inv' mul
   where _G  = [1,2,4,5,7,8] :: [Mod Integer 9]
         e   = 1 :: Mod Integer 9
+        inv' a = inv a :: Mod Integer 9
         mul a b = a * b :: Mod Integer 9
+
+first10IntegersOverAddition = newGroup _G e inv add
+ where _G = [1..10]
+       e  = 0
+       inv a = -a
+       add a b = a + b
 
 spec :: Spec
 spec = do
-  describe "Groups.verify" $ do
+  describe "Groups.isGroup" $ do
     it "returns true for the multiplicative group of integers modulo 9." $ do
-      Groups.verify multiplicativeGroupOfIntegersModulo9 `shouldBe` (True, "")
+      isGroup multiplicativeGroupOfIntegersModulo9 `shouldBe` (True, "")
+    it "returns false for the first 10 positive integers over addition." $ do
+      isGroup first10IntegersOverAddition `shouldNotBe` (True, "")
 
-
+  describe "Groups.isAbelianGroup" $ do
+    it "returns true for the multiplicative group of integers modulo 9." $ do
+      isAbelianGroup multiplicativeGroupOfIntegersModulo9 `shouldBe` (True, "")
