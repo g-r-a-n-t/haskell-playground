631e52b38e51ceb58e75ef98a5c7180c11f9b47f
Grant Wuerker <gwuerker@gmail.com>
2019-08-15 18:05:04 +0200

    map qualities and isomorphisms

--- algebraic-structures/src/Groups.hs
+++ algebraic-structures/src/Groups.hs
@@ -18,6 +18,11 @@ newGroup _S e inv op = Group _S e inv op
 data Homomorphism a b = Homomorphism (a -> b) (Group a) (Group b)
 newHomomorphism f _G _H = Homomorphism f _G _H
 
+-- Group isomorphisms consist of the following elements: two groups `Group a` and `Group b` and a map from the first
+-- set to the second `(a -> b)`.
+data Isomorphism a b = Isomorphism (a -> b) (Group a) (Group b)
+newIsomorphism f _G _H = Isomorphism f _G _H
+
 -- Verifies that the elements do in fact form a Group algebra.
 -- This is only computationally feasible on small carrier sets.
 isGroup :: Eq a => Group a -> (Bool, String)
@@ -40,9 +45,18 @@ isAbelianGroup (Group _S e inv op)
 -- This assumes each of the groups provided are valid.
 isHomomorphic :: Eq a => Eq b => Homomorphism a b -> (Bool, String)
 isHomomorphic (Homomorphism f _G _H)
-  | not $ all (\a -> elem (f a) _Sh) _Sg = (False, "The function maps an element of G to an element outside of H.")
+  | not $ isGeneralMap f _Sg _Sh = (False, "Not a general map.")
   | not $ all (\(a, b) -> f (opG a b) == opH (f a) (f b)) pairs = (False, "The equality check failed.")
   | otherwise = (True, "")
   where pairs = [(a, b) | a <- _Sg, b <- _Sg]
         Group _Sg _ _ opG = _G
         Group _Sh _ _ opH = _H
+
+isIsomorphic :: Eq a => Eq b => Isomorphism a b -> (Bool, String)
+isIsomorphic (Isomorphism f _G _H)
+  | not $ isHomoRes = (False, isHomoErr)
+  | not $ isBijective f _Sg _Sh = (False, "The mapping function is not bijective.")
+  | otherwise = (True, "")
+  where (isHomoRes, isHomoErr) = isHomomorphic (newHomomorphism f _G _H)
+        Group _Sg _ _ _ = _G
+        Group _Sh _ _ _ = _H
--- algebraic-structures/src/Qualities.hs
+++ algebraic-structures/src/Qualities.hs
@@ -3,8 +3,14 @@ module Qualities (
   isAssociative,
   isCommunicative,
   hasIdentity,
-  isInvertible
+  isInvertible,
+  isGeneralMap,
+  isBijective,
+  isSurjective,
+  isInjective
 ) where
+
+import Data.List.Unique
 
 hasClosure :: Eq a => [a] -> (a -> a -> a) -> Bool
 hasClosure _S op = and [elem (op a b) _S | (a, b) <- pairs]
@@ -28,3 +34,24 @@ isInvertible _S e inv op
  | not $ all (\(_, i) -> elem i _S) pairs = False                -- -a <- A, a <- A
  | otherwise = all (\(a, i) -> op a i == e && op i a == e) pairs -- a * i = i * a = e, a <- A
   where pairs = [(a, inv a) | a <- _S]
+
+-- All elements of f(A) map to an element in B.
+isGeneralMap :: Eq a => Eq b => (a -> b) -> [a] -> [b] -> Bool
+isGeneralMap f _A _B = all (\a -> elem (f a) _B) _A
+
+-- All elements of B are contained in f(A).
+isSurjective :: Eq a => Eq b => (a -> b) -> [a] -> [b] -> Bool
+isSurjective f _A _B
+  | not $ isGeneralMap f _A _B = False
+  | otherwise = all (\a' -> elem a' _B) _A'
+  where _A' = map f _A
+
+-- All elements of f(A) occur once.
+isInjective :: Eq a => Eq b => (a -> b) -> [a] -> [b] -> Bool
+isInjective f _A _B
+  | not $ isGeneralMap f _A _B = False
+  | otherwise = all (\a' -> countElem a' _A' == 1) _A'
+  where _A' = map f _A
+
+isBijective :: Eq a => Eq b => (a -> b) -> [a] -> [b] -> Bool
+isBijective f _A _B = isInjective f _A _B && isBijective f _A _B
